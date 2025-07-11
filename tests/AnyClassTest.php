5493a10f1ac512dc2a94a399230b28e912b5c41f
Andrei Barabash <andrey.barabash@mendep.ru>
2025-07-11 23:17:29 +0300

    Add test

---
+++ tests/AnyClassTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class AnyClassTest extends TestCase
+{
+    public function testFoo()
+    {
+        $this->assertTrue(1 === 1);
+    }
+}
